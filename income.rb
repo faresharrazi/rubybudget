4c0e2f3fa62ded01cab0852a04eab16a2d04bdee
Fares Harrazi <m.fares.harrazi@gmail.com>
2024-10-22 11:58:12 +0300

    add income class

---
+++ income.rb
@@ -0,0 +1,28 @@
+class Income
+  attr_accessor :id, :name, :amount, :type
+
+  @@next_id = 1  # Class variable to auto-increment IDs
+  @@valid_types = ['Salary', 'Rent', 'Other']  # Default income types
+
+  def initialize(name, amount, type)
+    @id = @@next_id
+    @@next_id += 1
+    @name = name
+    @amount = amount
+    if @@valid_types.include?(type)
+      @type = type
+    else
+      raise "Invalid income type. Valid types are: #{@@valid_types.join(', ')}"
+    end
+  end
+
+  # Class method to add new types of income
+  def self.add_type(new_type)
+    @@valid_types << new_type unless @@valid_types.include?(new_type)
+  end
+
+  # Class method to list all valid types
+  def self.types
+    @@valid_types
+  end
+end
