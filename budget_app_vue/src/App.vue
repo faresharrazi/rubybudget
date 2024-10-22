cdb6caf9adb394730044240c1285fc501da8002a
Fares Harrazi <m.fares.harrazi@gmail.com>
2024-10-22 13:21:40 +0300

    update the app.vue

--- budget_app_vue/src/App.vue
+++ budget_app_vue/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Income />
+    <Outcome />
+    <Account />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Income from "./components/Income.vue";
+import Outcome from "./components/Outcome.vue";
+import Account from "./components/Account.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Income,
+    Outcome,
+    Account,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
