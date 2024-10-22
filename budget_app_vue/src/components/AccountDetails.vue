58f5b3952fba2c370702e4fc6a0ae6769c963434
Fares Harrazi <m.fares.harrazi@gmail.com>
2024-10-22 13:35:21 +0300

    rename the components

---
+++ budget_app_vue/src/components/AccountDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Accounts</h2>
+    <ul>
+      <li v-for="account in accounts" :key="account.id">
+        {{ account.name }}: {{ account.amount }} EUR (Fraction:
+        {{ account.fraction }})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccountDetails",
+  data() {
+    return {
+      accounts: [
+        { id: 1, name: "Necessities", amount: 500, fraction: 0.5 },
+        { id: 2, name: "Savings", amount: 200, fraction: 0.2 },
+        { id: 3, name: "Investments", amount: 300, fraction: 0.3 },
+      ],
+    };
+  },
+};
+</script>
---
+++ budget_app_vue/src/components/IncomeSource.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h2>Add Income</h2>
+    <form @submit.prevent="addIncome">
+      <label for="name">Income Name:</label>
+      <input v-model="name" id="name" />
+
+      <label for="amount">Amount:</label>
+      <input v-model="amount" id="amount" type="number" />
+
+      <label for="type">Type:</label>
+      <select v-model="type">
+        <option v-for="option in incomeTypes" :key="option">
+          {{ option }}
+        </option>
+      </select>
+
+      <button type="submit">Add Income</button>
+    </form>
+
+    <div v-if="incomes.length">
+      <h3>Incomes List</h3>
+      <ul>
+        <li v-for="income in incomes" :key="income.id">
+          {{ income.name }}: {{ income.amount }} EUR ({{ income.type }})
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IncomeSource",
+  data() {
+    return {
+      name: "",
+      amount: 0,
+      type: "Salary",
+      incomeTypes: ["Salary", "Rent", "Other"],
+      incomes: [],
+    };
+  },
+  methods: {
+    addIncome() {
+      const newIncome = {
+        id: Date.now(),
+        name: this.name,
+        amount: this.amount,
+        type: this.type,
+      };
+      this.incomes.push(newIncome);
+      // Reset form
+      this.name = "";
+      this.amount = 0;
+      this.type = "Salary";
+    },
+  },
+};
+</script>
